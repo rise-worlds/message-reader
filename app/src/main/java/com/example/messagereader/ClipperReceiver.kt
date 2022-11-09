6bc3cac9e78454c73a786125ddaf1dbec53d119c
rise <rise.worlds@outlook.com>
2022-11-09 16:14:39 +0800

    优化保存到剪贴板

--- app/src/main/java/com/example/messagereader/ClipperReceiver.kt
+++ app/src/main/java/com/example/messagereader/ClipperReceiver.kt
@@ -1,54 +1,45 @@
 package com.example.messagereader
 
 import android.app.Activity
-import android.content.BroadcastReceiver
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import android.util.Log
 
-
+// am broadcast -n com.example.messagereader/.ClipperReceiver -a clipper.set -e "text" "电脑上的内容"
+// am broadcast -n com.example.messagereader/.ClipperReceiver -a clipper.get -esn "text"
 class ClipperReceiver : BroadcastReceiver() {
     private val TAG = "ClipboardReceiver"
 
-    var ACTION_GET = "clipper.get"
-    var ACTION_GET_SHORT = "get"
-    var ACTION_SET = "clipper.set"
-    var ACTION_SET_SHORT = "set"
-    var EXTRA_TEXT = "text"
-
-    private fun isActionGet(action: String?): Boolean {
-        return ACTION_GET == action || ACTION_GET_SHORT == action
-    }
-
-    private fun isActionSet(action: String?): Boolean {
-        return ACTION_SET == action || ACTION_SET_SHORT == action
-    }
+    private val ACTION_GET = "clipper.get"
+    private val ACTION_SET = "clipper.set"
+    private val EXTRA_TEXT = "text"
 
     override fun onReceive(context: Context, intent: Intent) {
         val cb: ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        if (isActionSet(intent.action)) {
+        if (ACTION_SET == intent.action) {
             Log.d(TAG, "Setting text into clipboard")
             val text = intent.getStringExtra(EXTRA_TEXT)
             if (text != null) {
-                cb.text = text
+                val clipData = ClipData.newPlainText("text", text)
+                cb.setPrimaryClip(clipData)
                 resultCode = Activity.RESULT_OK
                 resultData = "Text is copied into clipboard."
             } else {
                 resultCode = Activity.RESULT_CANCELED
                 resultData = "No text is provided. Use -e text \"text to be pasted\""
             }
-        } else if (isActionGet(intent.action)) {
+        } else if (ACTION_GET == intent.action) {
             Log.d(TAG, "Getting text from clipboard")
-            val clip: CharSequence = cb.text
+            val clip: ClipData? = cb.primaryClip
             if (clip != null) {
                 Log.d(TAG, String.format("Clipboard text: %s", clip))
                 resultCode = Activity.RESULT_OK
-                resultData = clip.toString()
+                resultData = (clip.getItemAt(0).text as String?)!!
             } else {
                 resultCode = Activity.RESULT_CANCELED
                 resultData = ""
             }
+        } else {
+            Log.d(TAG, "unknown action")
         }
     }
 }
